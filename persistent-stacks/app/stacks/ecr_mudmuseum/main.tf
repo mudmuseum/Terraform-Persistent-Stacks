e28cacd912cf1c4919289f9b5ace1689599fa3c5
Nicholas Warner <nswarner2@gmail.com>
2021-04-15 07:37:12 -0400

    Added The Gathering as an ECR.

--- persistent-stacks/app/stacks/ecr_mudmuseum/main.tf
+++ persistent-stacks/app/stacks/ecr_mudmuseum/main.tf
@@ -2,5 +2,6 @@ module "ecr_mudmuseum" {
   source = "github.com/mudmuseum/terraform-modules.git//modules/elastic_container_registry"
 
   names  = [ "rom-2.4.b4",
-             "dystopiagold" ]
+             "dystopiagold",
+             "gathering-1.0.1" ]
 }
