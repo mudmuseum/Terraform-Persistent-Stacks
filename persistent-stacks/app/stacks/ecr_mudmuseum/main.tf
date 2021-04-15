c2db8cfce70930884b1cc18b0879c3d350e8fc75
Nicholas Warner <nswarner2@gmail.com>
2021-04-15 15:32:43 -0400

    Updated to allow tag support for SSH Key Pairs and ECR.

--- persistent-stacks/app/stacks/ecr_mudmuseum/main.tf
+++ persistent-stacks/app/stacks/ecr_mudmuseum/main.tf
@@ -1,7 +1,33 @@
+########################
+#                      #
+#      Setup Tags      #
+#                      #
+########################
+
+data "aws_region" "current-region" {}
+
+locals {
+  tags = {
+    "cost-center"        = "mudmuseum"
+    "mm:resource-type"   = "aws_ecr_repository"
+    "mm:resource-region" = data.aws_region.current-region.name
+    "mm:project"         = "pipeline-build-push"
+    "mm:environment"     = "persistent"
+  }
+}
+
+########################################################
+#                                                      #
+#      Root Module - Elastic Container Repository      #
+#                                                      #
+########################################################
+
 module "ecr_mudmuseum" {
-  source = "github.com/mudmuseum/terraform-modules.git//modules/elastic_container_registry"
+  source = "github.com/mudmuseum/terraform-modules.git//modules/elastic_container_registry?ref=v0.2.8"
 
   names  = [ "rom-2.4.b4",
              "dystopiagold",
              "gathering-1.0.1" ]
+
+  tags   = local.tags
 }
